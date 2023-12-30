4c06673badca164216ed9e1e4bcf3840b2174abf
1214903572@qq.com <1214903572@qq.com>
2023-12-30 11:44:22 +0800

    提交

--- src/main/java/com/zhengjun/springbootproject01/Configration/MyMvcConfig.java
+++ src/main/java/com/zhengjun/springbootproject01/Configration/MyMvcConfig.java
@@ -2,6 +2,7 @@ package com.zhengjun.springbootproject01.Configration;
 
 import com.zhengjun.springbootproject01.ServiceImpliment.InterceptorImp;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class MyMvcConfig implements WebMvcConfigurer {
@@ -9,4 +10,13 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new InterceptorImp()).addPathPatterns("/**").excludePathPatterns("/","/user/login","/asserts/**","/webjars/**");
     }
+
+   @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("/index");
+        registry.addViewController("/login").setViewName("forward:/index.html");
+
+    }
+
 }
